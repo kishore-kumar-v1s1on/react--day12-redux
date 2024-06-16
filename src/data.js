ff10da54793be43653436c78f21d27e18648d112
Gopi <GopiR5@yahoo.com>
2024-06-16 18:07:45 +0530

    react-day12-redux

---
+++ src/data.js
@@ -0,0 +1,96 @@
+// export const items = [
+//   {
+//     id: 1,
+//     name: "Blue T-Shirt",
+//     price: 20,
+//     quantity: 1,
+//     image:
+//       "https://images.unsplash.com/photo-1542291026-7eec264c27ff?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8bmlrZXxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60",
+//   },
+//   {
+//     id: 2,
+//     name: "Red Hoodie",
+//     price: 35.0,
+//     quantity: 2,
+//     image:
+//       "https://cdn.pixabay.com/photo/2016/12/10/16/57/shoes-1897708__480.jpg",
+//   },
+//   {
+//     id: 3,
+//     name: "Black Jeans",
+//     price: 50.0,
+//     quantity: 1,
+//     image:
+//       "https://cdn.pixabay.com/photo/2016/03/27/22/16/fashion-1284496_1280.jpg",
+//   },
+//   {
+//     id: 4,
+//     name: "Red hoodie",
+//     price: 50.0,
+//     quantity: 1,
+//     image:
+//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQl1qO-BkqC8LbaSpcBs3tFZuX9RRh8AdSCIQ&usqp=CAU",
+//   },
+//   {
+//     id: 5,
+//     name: "Red hoodie",
+//     price: 50.0,
+//     quantity: 1,
+//     image:
+//       "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTaYBmqHsFLBrm0zMV9LvNf3tUAZHhSXGi1Ng&usqp=CAU",
+//   },
+//   {
+//     id: 6,
+//     name: "T-shirt",
+//     price: 50.0,
+//     quantity: 1,
+//     image:
+//       "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&w=600",
+//   },
+// ];
+
+
+export const items = [
+  {
+    "id": 1,
+    "name": "iPhone 9",
+    "price": 549,
+quantity: 1,
+    "image":  "https://i.dummyjson.com/data/products/1/1.jpg",
+},
+{
+    "id": 2,
+    "name": "iPhone X",
+    "price": 899,
+quantity: 1,
+    "image": "https://i.dummyjson.com/data/products/2/1.jpg",
+},
+{
+    "id": 3,
+    "name": "Samsung Universe 9",
+    "price": 1249,
+quantity: 1,
+     "image":   "https://i.dummyjson.com/data/products/3/1.jpg",
+},
+{
+    "id": 4,
+    "name": "OPPOF19",
+    "price": 280,
+quantity: 1,
+"image":  "https://i.dummyjson.com/data/products/4/1.jpg",
+},
+{
+    "id": 5,
+    "name": "Huawei P30",
+    "price": 499,
+quantity: 1,
+     "image":  "https://i.dummyjson.com/data/products/5/1.jpg",
+},
+{
+id: 6,
+name: "T-shirt",
+price: 50.0,
+quantity: 1,
+image:      "https://images.pexels.com/photos/8532616/pexels-photo-8532616.jpeg?auto=compress&cs=tinysrgb&w=600",
+},
+];
